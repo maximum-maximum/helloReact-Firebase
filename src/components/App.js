bb63fd1e5c773994ebf2d2bb2a065db2fe0e2c1e
maximum-maximum <mr_ver74@yahoo.co.jp>
2021-12-16 21:14:24 +0900

    init

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { pushMessage } from "../firebase";
+
+const App = () => {
+  const [name, setName] = useState("default");
+  const [text, setText] = useState("text");
+  return (
+    <>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName((name) => (name = e.target.value))}
+      />
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText((text) => (text = e.target.value))}
+      />
+      <button onClick={() => pushMessage({ name: name, text: text })}>
+        push
+      </button>
+    </>
+  );
+};
+
+export default App;
